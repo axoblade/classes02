a57ef82431553d00588e4d2556bf20be87635d71
axoblade <axoblade@gmail.com>
2021-07-27 13:48:14 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassesAndObjects
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(entryBox.Text))
+            {
+                MessageBox.Show("Please provide the first name", "Name error 01");
+            }
+            else if(string.IsNullOrEmpty(lastNameEntry.Text.ToString())){
+                MessageBox.Show("Please provide the last name", "Name error 02");
+            }
+            else
+            {
+                string lastNameString = middleNameEntry.Text != null ? middleNameEntry.Text.ToString() : "noMiddleName";
+
+                string firstName = entryBox.Text.ToString();
+                string lastName = lastNameEntry.Text.ToString();
+
+                FullNames fullNames = new FullNames();
+
+                string full = fullNames.fullnames(firstName,lastNameString,lastName).ToString();
+                listBox.Items.Add(full);
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+    }
+
+    public class FullNames
+    {
+        public string fullnames(string firstName, string middleName, string lastName)
+        {
+            string fullName = firstName + " " + middleName + " " + lastName;
+
+            return fullName;
+        }
+    }
+}
